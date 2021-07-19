a346abd92f0109cb3b2df1c3b5db9169e72017a3
RabuAme <85229493+RabuAme@users.noreply.github.com>
2021-07-19 14:01:09 +0800

    Update Myconstants.py

--- Myconstants.py
+++ Myconstants.py
@@ -1,3 +1,3 @@
 NID = 'D0776487'  #務必保留引號
 Password = 'Qaz125280'  #務必保留引號
-Tesseract = 'D:/Program Files/Tesseract-OCR/tesseract.exe'
+Tesseract = 'D:/Program Files/Tesseract-OCR/tesseract.exe' #Tesseract安裝路徑
